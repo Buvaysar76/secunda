d8e8b088fccdfde22d06a0c0d4c004102e0a85be
Buvaysar <buvaysar2032@gmail.com>
2025-10-24 19:21:27 +0300

    feat: редирект с / на /admin

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Contracts\View\View;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', static fn(): View => view('app'));
+Route::get('/', static fn() => redirect('/admin'));
 
 Route::get('/api/docs', static fn(): View => view('swagger/ui', [
     'jsonUrl' => route('openapi.json'),
